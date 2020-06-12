09ee9d9431122916e5af68021a9929171538a096
Fiona-Mitchell <fionamitchelldesign@gmail.com>
2020-06-12 22:26:00 +0100

    created 3 parallax squares and connected them to my mouse movement
    I also added a data-depth to each square - next step is to code the depth in js

---
+++ js/js.js
@@ -0,0 +1,83 @@
+// html setup
+var itemsHTMLColletion = document.getElementsByClassName('parallax-item');
+var itemsArray = Array.from(itemsHTMLColletion);
+
+
+var input = {
+  mouseX: {
+    start: 0,
+    end: window.innerWidth,
+    current: 0,
+  },
+  mouseY: {
+    start: 0,
+    end: window.innerHeight,
+    current: 0
+  }
+};
+
+// This is an input set up
+input.mouseX.range = input.mouseX.end - input.mouseX.start;
+input.mouseY.range = input.mouseY.end - input.mouseY.start;
+
+
+
+// This is an output set up
+var output = {
+  x: {
+  start: -150,
+  end: 150,
+  current: 0,
+},
+  y: {
+    start: -150,
+    end: 150,
+    current: 0,
+  },
+};
+
+// This is output set up
+output.x.range = output.x.end - output.x.start;
+output.y.range = output.y.end - output.y.start;
+
+
+
+var handleMouseMove = function (event) {
+  // mouse x input
+  input.mouseX.current = event.clientX;
+  input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
+// mouse y input
+  input.mouseY.current = event.clientY;
+  input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
+
+// connect input to ouput
+
+// output x
+output.x.current = output.x.start + (input.mouseX.fraction * output.x.range)
+
+// output y
+output.y.current = output.y.start + (input.mouseY.fraction * output.y.range)
+
+//apply output to html
+itemsArray.forEach(function(item, k){
+
+    item.style.transform = 'translate('+output.x.current+'px, '+output.y.current+'px)';
+});
+
+
+// console.log('output.x.current', output.x.current)
+// console.log('fractionY', input.mouseY.fraction)}
+}
+
+var handleResize = function () {
+  input.mouseX.end = window.innerWidth;
+  input.mouseX.range = input.mouseX.end - input.mouseX.start;
+
+  input.mouseY.end = window.innerHeight;
+  input.mouseY.range = input.mouseY.end - input.mouseY.start;
+}
+
+
+
+window.addEventListener('mousemove', handleMouseMove)
+window.addEventListener('resize', handleResize)
